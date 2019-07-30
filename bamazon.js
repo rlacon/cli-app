ef42a2770ae019c5f737405f79bbac88c1df84d4
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-29 20:38:02 -0400

    Set up database and created table. Inserted item into table. JS file console.logs results of the table.

---
+++ bamazon.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "marsball",
+    database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+  function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
---
+++ bamazon_db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT default 0,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Shampoo', 'Health & Beauty', 10.95, 40); 
